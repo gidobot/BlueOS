1ef51f9ab92c76cd9e0c1e91089366c776f935c4
Gideon Billings <gideon.billings@sydney.edu.au>
2026-03-19 11:01:58 +1100

    Convenience script.

---
+++ build_docker.sh
@@ -0,0 +1,4 @@
+docker build -t gidobot/blueos-core:master \
+    --build-arg GIT_DESCRIBE_TAGS=$(git describe --long --always --dirty --all) \
+    --build-arg VITE_APP_GIT_DESCRIBE=$(git describe --long --always --dirty --all) \
+    core/
