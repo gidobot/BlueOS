5340e662ddb50fa7aaa550d5cec437e69f1727a3
Gideon Billings <gidobot@umich.edu>
2026-03-29 09:38:23 -0400

    Add shell chabang.

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 docker build -t gidobot/blueos-core:master \
     --build-arg GIT_DESCRIBE_TAGS=$(git describe --long --always --dirty --all) \
     --build-arg VITE_APP_GIT_DESCRIBE=$(git describe --long --always --dirty --all) \
