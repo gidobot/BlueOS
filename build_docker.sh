ac1464af5d8cb81320049d520c7fec3ac2ef6330
Gideon Billings <gidobot@umich.edu>
2026-03-29 14:22:58 -0400

    Updates to fix breaking git tags.

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+
 docker build -t gidobot/blueos-core:master \
-    --build-arg GIT_DESCRIBE_TAGS=$(git describe --long --always --dirty --all) \
-    --build-arg VITE_APP_GIT_DESCRIBE=$(git describe --long --always --dirty --all) \
+    --build-arg GIT_DESCRIBE_TAGS="$(git describe --long --always --dirty --all)" \
+    --build-arg VITE_APP_GIT_DESCRIBE="$(git describe --long --always --dirty --all)" \
     core/
